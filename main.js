3f941daacd2ae3cc434b4dde32c01721fb594ca5
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-06-05 19:04:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+prediction_1 = "";
+prediction_2 = "";
 Webcam.set({
     width: 400,
     height: 300,
@@ -9,7 +11,7 @@ Webcam.attach(camera);
 function capture() {
     Webcam.snap(
         function(data_url) {
-            document.getElementById("result").innerHTML = "<img src='"+data_url+"' id='new_image'>";
+            document.getElementById("result").innerHTML = "<img src='"+data_url+"' id='captured_image'>";
         }
     );
 }
@@ -17,4 +19,47 @@ console.log("ml5 version: ", ml5.version);
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/2tlJtpIlp/model.json", modelLoaded);
 function modelLoaded() {
     console.log("Model Loaded!");
+}
+function speak() {
+    var synth = window.speechSynthesis;
+    prediction_1 = "The 1st prediction is " + prediction_1;
+    prediction_2 = "The 2nd prediction is " + prediction_2;
+    var utterance = new SpeechSynthesisUtterance(prediction_1 + prediction_2);
+    synth.speak(utterance);
+}
+//speak();
+function identify() {
+    new_image = document.getElementById("captured_image");
+    classifier.classify(new_image, getResult);
+}
+function getResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        document.getElementById("hand_gesture_name_1").innerHTML = prediction_1;
+        document.getElementById("hand_gesture_name_2").innerHTML = prediction_2;
+        if (prediction_1 == "Victory") {
+            document.getElementById("hand_gesture_symbol_1").innerHTML = "&#9996;";
+        }
+        if (prediction_1 == "Amazing") {
+            document.getElementById("hand_gesture_symbol_1").innerHTML = "&#128076;";
+        }
+        if (prediction_1 == "Best") {
+            document.getElementById("hand_gesture_symbol_1").innerHTML = "&#128077;";
+        }
+        if (prediction_2 == "Victory") {
+            document.getElementById("hand_gesture_symbol_2").innerHTML = "&#9996;";
+        }
+        if (prediction_2 == "Amazing") {
+            document.getElementById("hand_gesture_symbol_2").innerHTML = "&#128076;";
+        }
+        if (prediction_2 == "Best") {
+            document.getElementById("hand_gesture_symbol_2").innerHTML = "&#128077;";
+        }
+        speak();
+    }
 }
